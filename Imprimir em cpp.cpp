f9c2750cdeae048b29dd5658353f035e4871fc15
Theo Carneiro <carneiro.theopm@gmail.com>
2024-05-29 17:52:40 -0300

    Add files via upload

---
+++ Imprimir em cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    std::cout<<"TTTTTTTTTTT     HHHH   HHHH     EEEEEEEEEEE         OOO    \n";
+    std::cout<<"    TT          HHHH   HHHH     EE                O     O  \n";
+    std::cout<<"    TT          HHHHHHHHHHH     EEEEEEEEE         O     O  \n";
+    std::cout<<"    TT          HHHH   HHHH     EE                O     O  \n";
+    std::cout<<"    TT          HHHH   HHHH     EEEEEEEEEEE         OOO    \n";
+    return 0;
+
+}
